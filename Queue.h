d53a6c268b7962e68a708cfc2dd2f391f7f9848a
Kateryna Doroftei <dix.collab@gmail.com>
2025-05-11 12:38:09 +0300

    add queue with its basic operations

---
+++ Queue.h
@@ -0,0 +1,69 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+#include <iostream>
+
+class Queue {
+private:
+    T* array;
+    int capacity;;
+    int front;
+    int rear;
+    int size;
+
+    public:
+    explicit Queue(int capacity) : capacity(capacity), front(0), rear(0), size(0)
+    {
+        array = new T[capacity];
+    }
+    ~Queue()
+    {
+        delete[] array;
+    }
+
+    [[nodiscard]] bool isEmpty() const
+    {
+        return size == 0;
+    }
+
+    [[nodiscard]] bool isFull() const
+    {
+        return size == capacity;
+    }
+
+    void Enqueue(const T& element)
+    {
+        if (isFull())
+        {
+            std::cout << "Queue is full." << std::endl;
+            return;
+        }
+        rear = (rear + 1) % capacity;
+        array[rear] = element;
+        size++;
+    }
+
+    T Dequeue()
+    {
+        if (isEmpty())
+        {
+            std::cout << "Queue is empty." << std::endl;
+            exit(1);
+        }
+        T item = array[front];
+        front = (front + 1) % capacity;
+        size--;
+        return item;
+    }
+
+    [[nodiscard]] T Peek() const
+    {
+        if (isEmpty())
+        {
+            std::cout << "Queue is empty." << std::endl;
+            exit(1);
+        }
+        return array[front];
+    }
+};
+
+#endif //QUEUE_H
